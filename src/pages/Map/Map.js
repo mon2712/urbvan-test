5b4ba039795620e8ce29c1577d12b45ae7ba7325
Montse <montze94@gmail.com>
2020-06-21 18:21:33 -0500

    Adds inputs to receive origin and destination in Map page

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -1,18 +1,73 @@
-import React from "react"
+import React, { useState } from "react"
 
+import { Container, Row, Form } from "react-bootstrap"
 import Page from "components/Page"
 import GoogleMap from "components/GoogleMap"
+import {
+  StyledLogo,
+  ContainerMap,
+  ContainerForm,
+  StyledButton,
+} from "./Map.styles.js"
 
 const MapContainer = (props) => {
-  
+  const [origin, setOrigin] = useState("Chicago, IL")
+  const [destination, setDestination] = useState("Los Angeles")
+  const [directions, setDirections] = useState({ origin, destination })
+
+  const handleChange = (e) => {
+    if (e.target.name === "origin") {
+      setOrigin(e.target.value)
+    }
+    if (e.target.name === "destination") {
+      setDestination(e.target.value)
+    }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setDirections({ origin, destination })
+  }
+
   return (
-    <Page title="404">
-      <GoogleMap
-        isMarkerShown
-        travelMode='DRIVING'
-        origin={'Chicago, IL'}
-        destination={'Los Angeles, CA'}
-      />
+    <Page title="mapa">
+      <Container fluid>
+        <Row>
+          <ContainerForm lg={4}>
+            <StyledLogo />
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Origen: </Form.Label>
+                <Form.Control
+                  name="origin"
+                  value={origin}
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Origen"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Destino: </Form.Label>
+                <Form.Control
+                  name="destination"
+                  value={destination}
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Destino"
+                />
+              </Form.Group>
+
+              <StyledButton variant="primary" type="submit">
+                BUSCAR RUTA
+              </StyledButton>
+            </Form>
+          </ContainerForm>
+          <ContainerMap lg={8}>
+            <GoogleMap isMarkerShown travelMode="DRIVING" location={directions} />
+          </ContainerMap>
+        </Row>
+      </Container>
     </Page>
   )
 }
---
+++ src/pages/Map/Map.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import { ReactComponent as Logo } from "pages/Map/img/logo.svg"
+import { Col, Button } from "react-bootstrap"
+
+export const StyledLogo = styled(Logo)`
+  width: 6rem;
+  margin-bottom: 2rem;
+`
+
+export const ContainerForm = styled(Col)`
+  padding: 2rem;
+`
+
+export const ContainerMap = styled(Col)`
+  padding: 0;
+`
+
+export const StyledButton = styled(Button)`
+  width: 100%;
+`
